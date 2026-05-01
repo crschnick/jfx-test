6157a577cf1d09dd3edefcd2955715e59996fa22
Christopher Schnick <crschnick@xpipe.io>
2026-05-01 08:23:26 +0200

    Add menu bar test

---
+++ src/main/java/io/xpipe/jfx_runner/MenuBar.java
@@ -0,0 +1,25 @@
+package io.xpipe.jfx_runner;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class MenuBar extends Application {
+
+    static void main() {
+        // Initialize AWT
+        GraphicsEnvironment.getLocalGraphicsEnvironment();
+        // Initialize JavaFX
+        Application.launch(MenuBar.class);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+
+        Desktop.getDesktop().setPreferencesHandler(e -> {
+
+        });
+    }
+}
